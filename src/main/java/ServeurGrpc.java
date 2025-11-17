3131201c7dff318c75d9cf4952c7aabec8d405a4
ElmehdiHachem <Elmehdihachem1@gmail.com>
2025-11-17 12:45:09 +0100

    gRPC

--- src/main/java/ServeurGrpc.java
+++ src/main/java/ServeurGrpc.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import services.UserService;
+
+import java.io.IOException;
 
 public class ServeurGrpc {
+    public static void main(String arg[]) throws IOException, InterruptedException {
+            Server server= ServerBuilder.forPort(9082).addService(new UserService()).build();
+            server.start();
+            System.out.println("Serveur demarre au port : "+server.getPort());
+            server.awaitTermination();
+        }
 }
--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,4 +1,21 @@
 package services;
 
-public class UserService {
+import io.grpc.stub.StreamObserver;
+import stub.User;
+import stub.userGrpc;
+
+public class UserService extends userGrpc.userImplBase{
+    @Override
+    public void login(User.LoginRequest request, StreamObserver<User.LoginResponse> responseObserver) {
+        String username=request.getUserName();
+        String password=request.getPassWord();
+        User.LoginResponse.Builder response= User.LoginResponse.newBuilder();
+        if(username.equals(password)) {
+            response.setIdResponse(1).setMsgResponse("SUCCES");
+        }
+        else
+            response.setIdResponse(-1).setMsgResponse("NON VALIDE");
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
+    }
 }
